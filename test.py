23adc328f08c508d67f347e00993421509e9285c
Gazi <gazi@Gazis-MacBook-Pro-2.local>
2024-10-01 14:37:48 +0100

    deleting test file

--- test.py
+++
@@ -1,7 +0,0 @@
-def salaryInput():
-    currency_format = input("Please provide what currency you get paid in e.g. '$', '£'").lower()
-    user_salary_input = int(input("Please input your full annual salary "))
-    user_final_salary = '{:,.2f}'.format(user_salary_input)
-    print(f"Your annual salary is {currency_format}{user_final_salary}")
-
-salaryInput()
