03d15cbb0cc6d36bc8997931295b95fc3fdc664d
Saif <admin@saifuddin.uk>
2024-10-01 15:06:21 +0100

    replace comma in input

--- src/salaryInput.py
+++ src/salaryInput.py
@@ -1,6 +1,6 @@
 def salaryInput():
     currency_format = input("Please provide what currency you get paid in e.g. '$', '£'").lower()
-    user_salary_input = int(input("Please input your full annual salary "))
+    user_salary_input = int(input("Please input your full annual salary ").replace(",", ""))
     user_final_salary = '{:,.2f}'.format(user_salary_input)
     print(f"Your annual salary is {currency_format}{user_final_salary}")
 
