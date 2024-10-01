1beb93a6e6a76d8d685a99aabfa7805e461bd10a
Gazi <gazi@Gazis-MacBook-Pro-2.local>
2024-10-01 14:34:49 +0100

    Testing salary

---
+++ test.py
@@ -0,0 +1,7 @@
+def salaryInput():
+    currency_format = input("Please provide what currency you get paid in e.g. '$', '£'").lower()
+    user_salary_input = int(input("Please input your full annual salary "))
+    user_final_salary = '{:,.2f}'.format(user_salary_input)
+    print(f"Your annual salary is {currency_format}{user_final_salary}")
+
+salaryInput()
