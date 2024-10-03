7da0e034ea248ddaa927c0b4a9fa92de07f457f6
Saif <admin@saifuddin.uk>
2024-10-03 13:18:14 +0100

    update Pots

--- src/pots.py
+++ src/pots.py
@@ -1,9 +1,12 @@
 class Pots:
-    def __init__(self, amount=0) -> None:
-        self.amount = amount
+    def __init__(self, name="new pot", currency="£", amount=0) -> None:
+        self.pot_name = name
+        self.pot_currency = currency
+        self.pot_amount = amount
+        self.pot_transactions = []
     
-    def deposit(self) -> None:
-        pass
+    def deposit(self, amount) -> None:
+        self.pot_amount += amount
 
-    def withdraw(self) -> None:
-        pass
+    def withdraw(self, amount) -> None:
+        self.pot_amount -= amount
