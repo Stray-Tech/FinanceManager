d6bb695a933ec0af25616f0984d8dd878216dd66
Saif <admin@saifuddin.uk>
2024-10-03 12:59:55 +0100

    add Pots class

---
+++ src/pots.py
@@ -0,0 +1,9 @@
+class Pots:
+    def __init__(self, amount=0) -> None:
+        self.amount = amount
+    
+    def deposit(self) -> None:
+        pass
+
+    def withdraw(self) -> None:
+        pass
