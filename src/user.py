6e12b36c565ecb9170f329464f86b8e365151f49
Saif <admin@saifuddin.uk>
2024-10-08 14:31:47 +0100

    add create and delete user functions

--- src/user.py
+++ src/user.py
@@ -1,9 +1,19 @@
-users = []
+from datetime import datetime
+
+users = {}
 
 class User:
     def __init__(self, name, balance) -> None:
+        self.ID = f"{len(users)+1}_{name}_{datetime.timestamp}"
         self.NAME = name
         self.balance = balance
+    
+    def get_id(self):
+        return self.ID
 
 def create_user(name, balance):
-    users.append(User(name, balance))
+    new_user = User(name, balance)
+    users[new_user.get_id()] = new_user
+
+def delete_user(ID):
+    del users[ID]
