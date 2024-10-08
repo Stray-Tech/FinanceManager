099c6d46a0a5bae22a71ed69b6a4d3c27339726b
Saif <admin@saifuddin.uk>
2024-10-08 14:49:22 +0100

    added docstring to User class

--- src/user.py
+++ src/user.py
@@ -3,17 +3,25 @@ from datetime import datetime
 users = {}
 
 class User:
-    def __init__(self, name, balance) -> None:
+    """This is a class for Users.
+
+    Arguments:
+        name {string} -- name of the user
+        balance {number} -- initial balance from all current accounts
+    """  
+    def __init__(self, name:str, balance:float) -> None:
         self.ID = f"{len(users)+1}_{name}_{datetime.timestamp}"
         self.NAME = name
         self.balance = balance
     
-    def get_id(self):
+    def get_id(self) -> str:
         return self.ID
 
-def create_user(name, balance):
+def create_user(name:str, balance:int) -> None:
     new_user = User(name, balance)
     users[new_user.get_id()] = new_user
 
-def delete_user(ID):
+def delete_user(ID:str) -> None:
     del users[ID]
+
+#
